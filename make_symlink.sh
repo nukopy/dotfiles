39cecdb9a51d1781bb1cd7840fbad7e8f1661790
nukopy <pytwbf201830@gmail.com>
2019-12-25 05:56:35 +0900

    Add karabiner setting files.

--- make_symlink.sh
+++ make_symlink.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# create symlink to home directory
 ln -sf $HOME/Projects/dotfiles/.zshenv.home ~/.zshenv
 ln -sf $HOME/Projects/dotfiles/latex/.latexmkrc ~/.latexmkrc
+ln -sf $HOME/Projects/dotfiles/karabiner ~/.config/karabiner
 
 exec $SHELL -l  # restart shell
