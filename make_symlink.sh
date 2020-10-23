75a4616dc87ec2a3b855f305f6542c66087cf6c2
nukopy <pytwbf201830@gmail.com>
2020-10-24 07:19:33 +0900

    add: Starship setting file `starship.toml` & create symlink

--- make_symlink.sh
+++ make_symlink.sh
@@ -6,5 +6,6 @@ ln -sf $HOME/Projects/dotfiles/.zshenv.home ~/.zshenv
 ln -sf $HOME/Projects/dotfiles/latex/.latexmkrc ~/.latexmkrc
 ln -sf $HOME/Projects/dotfiles/karabiner ~/.config/karabiner
 ln -sf $HOME/Projects/dotfiles/direnv/.direnvrc ~/.direnvrc
+ln -sf $HOME/Projects/dotfiles/starship/starship.toml ~/.config/starship.toml
 
 exec $SHELL -l  # restart shell
