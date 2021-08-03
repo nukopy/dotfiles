b92c90445fe06b8d6cd775816e5fc67740308664
nukopy <pytwbf201830@gmail.com>
2021-08-03 21:32:55 +0900

    chore: update .gitconfig

--- make_symlink.sh
+++ make_symlink.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # create symlink to home directory
-ln -sf $HOME/Projects/dotfiles/.gitconfig ~/.gitconfig
+ln -sf $HOME/Projects/dotfiles/gitconfig/.gitconfig ~/.gitconfig
+ln -sf $HOME/Projects/dotfiles/gitconfig/.gitconfig.nukopy ~/Projects/.gitconfig
+ln -sf $HOME/Projects/dotfiles/gitconfig/.gitconfig.neets13 ~/Projects_neets13/.gitconfig
 ln -sf $HOME/Projects/dotfiles/.zshenv.home ~/.zshenv
 ln -sf $HOME/Projects/dotfiles/latex/.latexmkrc ~/.latexmkrc
 ln -sf $HOME/Projects/dotfiles/karabiner ~/.config/karabiner
